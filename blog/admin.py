d7296169ddad69629486589195c32db73bbcc95b
НеПиковАтвечаю <gottsehedichsss@gmail.com>
2025-02-14 18:23:19 +0300

    did migrations and added new record to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Post
+from .models import Category, Post , Likes, Comments
 
 
 @admin.register(Category)
@@ -11,4 +11,15 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Likes)
+class LikesAdmin(admin.ModelAdmin):
+
+    list_display = ('category', 'date', 'reaction')
+    list_filter = ('category', 'date', 'reaction')
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
     ...
